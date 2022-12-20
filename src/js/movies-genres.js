e7941cf206259d3abced00206b641094d817fcdd
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-20 19:00:21 +0200

    Add movie genres fix

--- src/js/movies-genres.js
+++ src/js/movies-genres.js
@@ -3,20 +3,19 @@ export const movieGenres = {
   12: 'Adventure',
   16: 'Animation',
   878: 'Science Fiction',
-  // Comedy          35
-  // Crime           80
-  // Documentary     99
-  // Drama           18
-  // Family          10751
-  // Fantasy         14
-  // History         36
-  // Horror          27
-  // Music           10402
-  // Mystery         9648
-  // Romance         10749
-
-  // TV Movie        10770
-  // Thriller        53
-  // War             10752
-  // Western         37
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
 };
