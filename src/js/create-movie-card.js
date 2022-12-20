3a7b291db2dedab038b2e0637e1412e11e7e16c9
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-20 18:45:49 +0200

    Add card list

--- src/js/create-movie-card.js
+++ src/js/create-movie-card.js
@@ -1,16 +1,21 @@
 import { refs } from './refs';
+import { movieGenres } from './movies-genres';
 
-export default function createMovieCardMarkup(movies) {
+export default function createMovieListMarkup(movies) {
   const markup = movies
     .map(movie => {
       return `<li class="movie-card">
-        <img src="" alt="" class="movie-card__img" />
+        <img src="https://image.tmdb.org/t/p/original${
+          movie.poster_path
+        }" alt="Poster of ${movie.original_title}" class="movie-card__img" />
         <div class="movie-card__wrap">
           <div class="movie-card__info">
-            <p class="movie-card__name">{movieName}</p>
-            <p class="movie-card__genre">{movieGenre} | {year}</p>
+            <p class="movie-card__name">${movie.original_title}</p>
+            <p class="movie-card__genre">${movie.genre_ids.map(id => {
+              return ` ${movieGenres[id]}`;
+            })} | ${new Date(movie.release_date).getFullYear()}</p>
           </div>
-          <span class="movie-card__rating">{rating}</span>
+          <span class="movie-card__rating">${movie.vote_average}</span>
         </div>
       </li>`;
     })
---
+++ src/js/movies-genres.js
@@ -0,0 +1,22 @@
+export const movieGenres = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  878: 'Science Fiction',
+  // Comedy          35
+  // Crime           80
+  // Documentary     99
+  // Drama           18
+  // Family          10751
+  // Fantasy         14
+  // History         36
+  // Horror          27
+  // Music           10402
+  // Mystery         9648
+  // Romance         10749
+
+  // TV Movie        10770
+  // Thriller        53
+  // War             10752
+  // Western         37
+};
