60a97258b24b71b6df8f3ac8689589c3b9329146
Oleksandr Romanchuk <novator0511@gmail.com>
2022-12-21 14:26:29 +0200

    add functions for nav-changes

---
+++ src/js/firebase-auth/interface-change.js
@@ -0,0 +1,27 @@
+function loginFormNotify(notifyEl, string) {
+  notifyEl.textContent = string;
+  notifyEl.classList.add('show');
+
+  setTimeout(() => {
+    notifyEl.textContent = '';
+    notifyEl.classList.remove('show');
+  }, 5000);
+}
+
+function showElements(elements) {
+  elements.forEach(link => {
+    link.style.display = 'inline-block';
+  });
+}
+
+function hideElements(elements) {
+  elements.forEach(link => {
+    link.style.display = 'none';
+  });
+}
+
+function classToggle(element, action, className) {
+  element.classList[action](className);
+}
+
+export { loginFormNotify, showElements, hideElements, classToggle };
