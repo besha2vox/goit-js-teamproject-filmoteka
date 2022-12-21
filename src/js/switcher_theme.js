d507b11d185de3ad07c925131663341dfd5011a8
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-12-21 04:32:48 +0200

    swither_theme

---
+++ src/js/switcher_theme.js
@@ -0,0 +1,20 @@
+const refs = {
+    toggle: document.querySelector('.theme-switch__toggle'),
+    body: document.querySelector('body')
+};
+refs.body.classList.add("light-theme");
+refs.toggle.addEventListener('change', changeTheme);
+
+
+function changeTheme(evt) {
+    if (refs.body.classList.contains("light-theme")) {
+        refs.body.classList.remove("light-theme");
+        refs.body.classList.add("dark-theme");
+    } else {
+        refs.body.classList.remove("dark-theme");
+        refs.body.classList.add("light-theme");
+    }
+
+}
+
+export { refs, changeTheme };
