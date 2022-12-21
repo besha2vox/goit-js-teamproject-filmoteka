b70ac632097b47f5807d82c53c3ab77299b933f4
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-21 15:07:42 +0200

    Add fixes

--- src/js/create-movie-card.js
+++ src/js/create-movie-card.js
@@ -1,25 +1,30 @@
 import { refs } from './refs';
 import { movieGenres } from './movies-genres';
 
-export default function createMovieListMarkup(movies) {
+export default function createMovieCardMarkup(movies) {
+  debugger;
   const markup = movies
     .map(movie => {
-      return `<li class="movie-card">
+      return `<li class="movie-card" id="${movie.id}">
         <img src="https://image.tmdb.org/t/p/original${
           movie.poster_path
         }" alt="Poster of ${movie.original_title}" class="movie-card__img" />
-        <div class="movie-card__wrap">
           <div class="movie-card__info">
             <p class="movie-card__name">${movie.original_title}</p>
-            <p class="movie-card__genre">${movie.genre_ids.map(id => {
-              return ` ${movieGenres[id]}`;
-            })} | ${new Date(movie.release_date).getFullYear()}</p>
+            <div class="movie-card__wrap">
+              <p class="movie-card__genre">${movie.genre_ids.map(id => {
+                return ` ${movieGenres[id]}`;
+              })} | ${movie.release_date.split('-')[0]}</p>
+              <span class="movie-card__rating">${movie.vote_average.toFixed(
+                1
+              )}</span>
+            </div>
           </div>
-          <span class="movie-card__rating">${movie.vote_average}</span>
-        </div>
       </li>`;
     })
     .join('');
+
+  console.log(markup);
 
   refs.movieList.innerHTML += markup;
 }
