8127bd85d3c3e2da15508cbb690fd4b73575d8c7
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-12-21 20:55:56 +0200

    first changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,14 @@
+import { Modal } from './js/class-modal';
+
 const itemCardEl = document.querySelector('.movie-card');
 
-const getModalMarkup = ({ movie_title, movie_description, movie_vote, movie_popularity, movie_original_title, movie_genre, movie_img, movie_votes }) => `<div class="card-modal" data-modal>
-            <button type="button" class="card-modal-close-btn" data-modal-close>
-              <svg class="card-modal-close" width="30" height="30">
-                <use></use>
-              </svg>
-            </button>
+const modal = new Modal(option);
+
+const getModalMarkup = ({ overview, vote_average, popularity, original_title, genres, poster_path, vote_count }) => `
             <div class="modal-card-wrapper">
-              <img src="${movie_img}" alt="film photo" class="modal-card-film-img">
+              <img src="${poster_path}" alt="film photo" class="modal-card-film-img">
               <div>
-                <h2 class="modal-film-title">${movie_title}</h2>
+                <h2 class="modal-film-title">${original_title}</h2>
                 <div class="modal-card-list-wrapper">
                   <ul class="modal-list-info">
                     <li class="modal-info-item">Vote / Votes</li>
@@ -18,45 +17,18 @@ const getModalMarkup = ({ movie_title, movie_description, movie_vote, movie_popu
                     <li class="modal-info-item">Genre</li>
                   </ul>
                   <ul class="modal-card-answers-list">
-                    <li class="modal-card-list-item"><span class="modal-card-vote">${movie_vote}</span><span class="modal-vote-slash">/</span>${movie_votes}</li>
-                    <li class="modal-card-list-item">${movie_popularity}</li>
-                    <li class="modal-card-list-item">${movie_original_title}</li>
-                    <li class="modal-card-list-item">${movie_genre}</li>
+                    <li class="modal-card-list-item"><span class="modal-card-vote">${vote_average}</span><span class="modal-vote-slash">/</span>${vote_count}</li>
+                    <li class="modal-card-list-item">${popularity}</li>
+                    <li class="modal-card-list-item">${original_title}</li>
+                    <li class="modal-card-list-item">${genres}</li>
                   </ul>
                 </div>
                 <h3 class="modal-card-title-about">About</h3>
-                <p class="modal-card-about-text">${movie_description}</p>
+                <p class="modal-card-about-text">${overview}</p>
                 <button class="modal-card-watched-btn" type="button">add to watched</button>
                 <button type="button" class="modal-card-queue-btn">add to queue</button>
               </div>
-            </div>
-          </div>`;
-
-const markup = apiList.map(item => getModalMarkup(item));
-
-const renderModal = () => {
-    itemCardEl.insertAdjacentHTML('beforeend', markup.join(''))
-}
-
-// Openmodal
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('.movie-card'),
-    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
-    modal: document.querySelectorAll('[data-modal]'),
-  };
-  refs.openModalBtn.forEach(function (li) {
-    li.addEventListener('click', toggleModal);
-  })
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal(e) {
-    refs.modal.forEach(function (modalEl) {
-      modalEl.classList.toggle('is-hidden');
-    })
-    document.body.classList.toggle('no-scroll')
-  }
-})();
+            </div>`;
 
 
+modal.open(getModalMarkup());
