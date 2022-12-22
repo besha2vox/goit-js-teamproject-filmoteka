3ae055d24270b85a90a690d99815a70feb2f904c
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-12-22 04:32:40 +0200

    add storageTheme

--- src/js/switcher_theme.js
+++ src/js/switcher_theme.js
@@ -1,18 +1,35 @@
 const refs = {
     toggle: document.querySelector('.theme-switch__toggle'),
+    langSwitcher: document.querySelector('.language-switcher__checkbox'),
+    span: document.querySelector('.language-switcher__lang'),
 };
-document.body.classList.add("light-theme");
-refs.toggle.addEventListener('change', changeTheme);
 
-function changeTheme(evt) {
-    if (document.body.classList.contains("light-theme")) {
+const storageTheme = loadDataFromLocalSt("theme");
+if (storageTheme !== undefined) {
+    if (storageTheme === "dark") {
         document.body.classList.remove("light-theme");
         document.body.classList.add("dark-theme");
     } else {
         document.body.classList.remove("dark-theme");
         document.body.classList.add("light-theme");
     }
+} else {
+    document.body.classList.add("light-theme");
+}
 
+refs.toggle.addEventListener('change', changeTheme);
+
+
+function changeTheme(evt) {
+    if (document.body.classList.contains("light-theme")) {
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+        saveDataToLocalSt("theme", "dark");
+    } else {
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+        saveDataToLocalSt("theme", "light");
+    }
 }
 
 export { refs, changeTheme };
