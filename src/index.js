5da7c454c239fb1db9d206df15fc6efa5e8ecd3d
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-12-22 17:16:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './js/firebase-auth/login-modal';
 import './js/firebase-auth/auth-realization';
 import './js/loader&topScroll';
 import './js/create-movie-card';
+import './js/manipulation-with-api/get-tranding-movies';
 import './js/modal-team';
