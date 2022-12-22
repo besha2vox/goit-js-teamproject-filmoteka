b5b2390cf2d1916afa995ebe1712d9de48568bf7
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-12-22 02:57:27 +0200

    document.body instead of refs.body

--- src/js/switcher_theme.js
+++ src/js/switcher_theme.js
@@ -1,17 +1,16 @@
 const refs = {
     toggle: document.querySelector('.theme-switch__toggle'),
-    body: document.querySelector('body')
 };
-refs.body.classList.add("light-theme");
+document.body.classList.add("light-theme");
 refs.toggle.addEventListener('change', changeTheme);
 
 function changeTheme(evt) {
-    if (refs.body.classList.contains("light-theme")) {
-        refs.body.classList.remove("light-theme");
-        refs.body.classList.add("dark-theme");
+    if (document.body.classList.contains("light-theme")) {
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
     } else {
-        refs.body.classList.remove("dark-theme");
-        refs.body.classList.add("light-theme");
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
     }
 
 }
