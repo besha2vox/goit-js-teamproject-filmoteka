1dc70edaba8a0d79fee1a16a08687ed129c3e4fe
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-12-22 17:16:18 +0200

    Update get-tranding-movies.js

--- src/js/manipulation-with-api/get-tranding-movies.js
+++ src/js/manipulation-with-api/get-tranding-movies.js
@@ -1,11 +1,27 @@
-const moviesRef = document.querySelector('.films-list');
+import { createMovieCardMarkup } from '../create-movie-card';
+import { API } from '../api';
+
+const api = new API();
+
+const moviesRef = document.querySelector('.movies-grid__list');
 
 export async function getTranding() {
-  const movies = await api.fetchTrendingMovies('week');
-  const template = movies.results.map(galleryTemplate).join('');
-
-  moviesRef.innerHTML = template;
+  const movies = await api.getMovieLatest('week');
+  const template = movies.results
+    .map(async movie => {
+      const oneMovieTempl = await createMovieCardMarkup(movie);
+      console.log('oneMovieTempl', oneMovieTempl);
+      return await oneMovieTempl;
+    })
+    .join('');
+  return await template;
   //   renderPagination(movies.total_pages, refs.pagination, getTranding, api);
 }
 
-getTranding();
+export async function render() {
+  const template = await getTranding();
+  moviesRef.innerHTML = template;
+  console.log('moviesRef', moviesRef);
+}
+
+render();
