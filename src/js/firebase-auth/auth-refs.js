befac282ab122fa1d9c04e95ab0c111f7747d3b3
Oleksandr Romanchuk <novator0511@gmail.com>
2022-12-22 12:54:01 +0200

    get login-modal with class

--- src/js/firebase-auth/auth-refs.js
+++ src/js/firebase-auth/auth-refs.js
@@ -5,20 +5,8 @@ const loginLink = document.getElementById('link__login');
 const logoutLink = document.getElementById('link__logout');
 const devider = document.getElementById('devider');
 const greeting = document.querySelector('.greeting');
-const enterModal = document.getElementById('enter-modal');
-const modalAuth = document.getElementById('modal__auth');
-const signupForm = document.querySelector('.form__signup');
-const loginForm = document.querySelector('.form__login');
-const signupFormTitle = document.getElementById('signup');
-const loginFormTitle = document.getElementById('login');
 const loginLinks = document.querySelectorAll('.login-user');
 const logoutLinks = document.querySelectorAll('.logout-user');
-const googleLogin = document.querySelectorAll(
-  '.auth-providers__link--google-login'
-);
-const facebookLogin = document.querySelectorAll(
-  '.auth-providers__link--facebook-login'
-);
 
 export {
   siteNav,
@@ -27,15 +15,7 @@ export {
   loginLink,
   logoutLink,
   devider,
-  enterModal,
-  modalAuth,
-  loginFormTitle,
-  signupFormTitle,
-  signupForm,
-  loginForm,
   greeting,
   loginLinks,
   logoutLinks,
-  googleLogin,
-  facebookLogin,
 };
