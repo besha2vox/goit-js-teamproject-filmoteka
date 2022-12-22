42a1382a8ab9c6b0feddd84e9eb13278e87073fa
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-22 08:52:05 +0200

    Add createMovieCardMarkup fx

--- src/js/create-movie-card.js
+++ src/js/create-movie-card.js
@@ -1,30 +1,44 @@
-import { refs } from './refs';
-import { movieGenres } from './movies-genres';
+import { API } from './api';
 
-export default function createMovieCardMarkup(movies) {
+const api = new API();
+
+const film = {
+  id: 123,
+  poster_path:
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTabby_cat&psig=AOvVaw1YzjkPs-O7Y0KdoXsv6ab0&ust=1671732711651000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOi2reWni_wCFQAAAAAdAAAAABAE',
+  title: 'Cat wars',
+  release_date: '2022-08-09',
+  vote_average: 7.0876,
+  ids: [28, 12, 16],
+};
+
+export default function createMovieCardMarkup(movie) {
+  function searchGenres(ids) {
+    const genres = api.getGenres();
+    const genresArr = ids.map(id => genres.find(genre => genre.id === id));
+    const genresNames = genresArr
+      .map(({ name }) => name)
+      .slice(0, 2)
+      .join(',');
+  }
   debugger;
-  const markup = movies
-    .map(movie => {
-      return `<li class="movie-card" id="${movie.id}">
+
+  return `<li class="movie-card" id="${movie.id}">
         <img src="https://image.tmdb.org/t/p/original${
           movie.poster_path
-        }" alt="Poster of ${movie.original_title}" class="movie-card__img" />
+        }" alt="Poster of ${movie.title}" class="movie-card__img" />
+        <div class="movie-card__wrap">
           <div class="movie-card__info">
-            <p class="movie-card__name">${movie.original_title}</p>
-            <div class="movie-card__wrap">
-              <p class="movie-card__genre">${movie.genre_ids.map(id => {
-                return ` ${movieGenres[id]}`;
-              })} | ${movie.release_date.split('-')[0]}</p>
+            <p class="movie-card__name">${movie.title}</p>
+              <p class="movie-card__genre">${searchGenres(genre_ids)} | ${
+    movie.release_date.split('-')[0]
+  }</p>
+              </div>
               <span class="movie-card__rating">${movie.vote_average.toFixed(
                 1
               )}</span>
-            </div>
           </div>
       </li>`;
-    })
-    .join('');
-
-  console.log(markup);
-
-  refs.movieList.innerHTML += markup;
 }
+
+console.log(createMovieCardMarkup(film));
