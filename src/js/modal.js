9eaf063bf29de4dac640bdb97fc21756d6d3bfda
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-12-22 19:22:25 +0200

    changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,31 +4,38 @@ const itemCardEl = document.querySelector('.movie-card');
 
 const modal = new Modal(option);
 
-const getModalMarkup = ({ overview, vote_average, popularity, original_title, genres, poster_path, vote_count }) => `
-            <div class="modal-card-wrapper">
-              <img src="${poster_path}" alt="film photo" class="modal-card-film-img">
+const url = `https://image.tmdb.org/t/p/original${poster_path}`;
+
+const getModalMarkup = ({ overview, vote_average, popularity, original_title, genres, poster_path, vote_count, title }) => {
+  const genresNames = genres.map(genre => genre.name).join(' ');
+            return `
+              <img src="${url}${poster_path}" alt="film photo" class="modal-card-film-img">
               <div>
-                <h2 class="modal-film-title">${original_title}</h2>
-                <div class="modal-card-list-wrapper">
-                  <ul class="modal-list-info">
-                    <li class="modal-info-item">Vote / Votes</li>
-                    <li class="modal-info-item">Popularity</li>
-                    <li class="modal-info-item">Original Title</li>
-                    <li class="modal-info-item">Genre</li>
-                  </ul>
-                  <ul class="modal-card-answers-list">
-                    <li class="modal-card-list-item"><span class="modal-card-vote">${vote_average}</span><span class="modal-vote-slash">/</span>${vote_count}</li>
-                    <li class="modal-card-list-item">${popularity}</li>
-                    <li class="modal-card-list-item">${original_title}</li>
-                    <li class="modal-card-list-item">${genres}</li>
-                  </ul>
-                </div>
-                <h3 class="modal-card-title-about">About</h3>
-                <p class="modal-card-about-text">${overview}</p>
-                <button class="modal-card-watched-btn" type="button">add to watched</button>
-                <button type="button" class="modal-card-queue-btn">add to queue</button>
+              <h2 class="modal-card__title">${title}</h2>
+              <div class="modal-card-list-wrapper">
+                <ul class="modal-list-info">
+                  <li class="modal-card__item"><span class="modal-card__info">Vote / Votes</span>
+                    <div class="modal-card__value">
+                      <span class="modal-card__vote">${vote_average}</span>
+                      <span class="modal-card__slash">/</span>
+                      <span class="modal-card__value">${vote_count}</span>
+                    </div>
+                  </li>
+                  <li class="modal-card__item"><span class="modal-card__info">Popularity</span><span
+                      class="modal-card__value">${popularity}</span></li>
+                  <li class="modal-card__item"><span class="modal-card__info">Original Title</span
+                      class="modal-card__value">${original_title}<span></span></li>
+                  <li class="modal-card__item"><span class="modal-card__info">Genre</span><span
+                      class="modal-card__value">${genresNames.genre}</span></li>
+                </ul>
               </div>
-            </div>`;
+              <h3 class="modal-card-title-about">About</h3>
+              <p class="modal-card-about-text">${overview}</p>
+              <button class="modal-card-watched-btn" type="button">add to watched</button>
+              <button type="button" class="modal-card-queue-btn">add to queue</button>
+              </div>`;
+             
+}
 
 
 modal.open(getModalMarkup());
