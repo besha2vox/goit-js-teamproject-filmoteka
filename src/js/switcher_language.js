2129594d250b7ac753ed2e01368599eb62c20206
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-12-23 04:56:20 +0200

    works with Class

--- src/js/switcher_language.js
+++ src/js/switcher_language.js
@@ -1,72 +1,151 @@
 import { saveDataToLocalSt, loadDataFromLocalSt, removeDataFromLocalSt } from './tool-tip';
 
-const refs = {
+class LanguageSwitcher {
+constructor() {
+    this.refs = {
     langSwitcher: document.querySelector('.language-switcher__checkbox'),
     // switcherText: document.querySelector('.language-switcher__lang'),
-};
-let dataLanguage = document.querySelectorAll("[data-enlang]");
+    };
+    this.dataLanguage = document.querySelectorAll("[data-enlang]");
+}
 
-const storageLanguage = loadDataFromLocalSt("language");
-if (storageLanguage !== undefined) {
+init() {
+    const storageLanguage = loadDataFromLocalSt("language");
+    if (storageLanguage !== undefined) {
     if (storageLanguage === "EN") {
-        changeLanguageText("EN");   
-        changeLanguageClass("en");
+        this.changeLanguageText("EN");
+        this.changeLanguageClass("en");
     } else {
-        changeLanguageText("UA");   
-        changeLanguageClass("ua");
+        this.changeLanguageText("UA");
+        this.changeLanguageClass("ua");
     }
-} else {
-    changeLanguageText("UA");   
+    } else {
+    this.changeLanguageText("UA");
     document.body.classList.add("ua");
+    }
+
+    this.refs.langSwitcher.addEventListener('change', this.changeLanguage.bind(this));
 }
 
-refs.langSwitcher.addEventListener('change', changeLanguage);
-
-function changeLanguage(evt) {
+changeLanguage() {
     if (document.body.classList.contains("ua")) {
-        changeLanguageText("UA");   
-        changeLanguageLocalStr("UA");
-        changeLanguageClass("ua");
-    } else { 
-        changeLanguageText("EN");   
-        changeLanguageLocalStr("EN");
-        changeLanguageClass("en");
-}
-}
-
-function changeLanguageClass(language) {
-    if (language === 'ua') {
-        document.body.classList.remove("ua");
-        document.body.classList.add("en");
+    this.changeLanguageText("UA");
+    this.changeLanguageLocalStr("UA");
+    this.changeLanguageClass("ua");
     } else {
-        document.body.classList.remove("en");
-        document.body.classList.add("ua");
+    this.changeLanguageText("EN");
+    this.changeLanguageLocalStr("EN");
+    this.changeLanguageClass("en");
     }
 }
 
-function changeLanguageLocalStr(language) {
-    if (language === 'UA') {  
-        saveDataToLocalSt("language", "EN");
-    } else {  
-        saveDataToLocalSt("language", "UA");
-}
+changeLanguageClass(language) {
+    if (language === 'ua') {
+    document.body.classList.remove("ua");
+    document.body.classList.add("en");
+    } else {
+    document.body.classList.remove("en");
+    document.body.classList.add("ua");
+    }
 }
 
-function changeLanguageText(language) {
+changeLanguageLocalStr(language) {
     if (language === 'UA') {
-        dataLanguage.forEach((data) => {
-            data.textContent = data.dataset.enlang;
-            if (data.hasAttribute("placeholder")) {
-                data.placeholder = data.dataset.enlang;
-            }
-        }); 
+    saveDataToLocalSt("language", "EN");
+    } else {
+    saveDataToLocalSt("language", "UA");
+    }
+}
 
-    } else { 
-        dataLanguage.forEach((data) => {
-            data.textContent = data.dataset.ualang;
-            if (data.hasAttribute("placeholder")) {
-                data.placeholder = data.dataset.ualang;
-            }
-        });
+changeLanguageText(language) {
+    if (language === 'UA') {
+    this.dataLanguage.forEach((data) => {
+        data.textContent = data.dataset.enlang;
+        if (data.hasAttribute("placeholder")) {
+        data.placeholder = data.dataset.enlang;
+        }
+    });
+    } else {
+    this.dataLanguage.forEach((data) => {
+        data.textContent = data.dataset.ualang;
+        if (data.hasAttribute("placeholder")) {
+        data.placeholder = data.dataset.ualang;
+        }
+    });
+    }
 }
 }
+
+const languageSwitcher = new LanguageSwitcher();
+languageSwitcher.init();
+
+// const refs = {
+//     langSwitcher: document.querySelector('.language-switcher__checkbox'),
+//     // switcherText: document.querySelector('.language-switcher__lang'),
+// };
+// let dataLanguage = document.querySelectorAll("[data-enlang]");
+
+// const storageLanguage = loadDataFromLocalSt("language");
+// if (storageLanguage !== undefined) {
+//     if (storageLanguage === "EN") {
+//         changeLanguageText("EN");   
+//         changeLanguageClass("en");
+//     } else {
+//         changeLanguageText("UA");   
+//         changeLanguageClass("ua");
+//     }
+// } else {
+//     changeLanguageText("UA");   
+//     document.body.classList.add("ua");
+// }
+
+// refs.langSwitcher.addEventListener('change', changeLanguage);
+
+// function changeLanguage(evt) {
+//     if (document.body.classList.contains("ua")) {
+//         changeLanguageText("UA");   
+//         changeLanguageLocalStr("UA");
+//         changeLanguageClass("ua");
+//     } else { 
+//         changeLanguageText("EN");   
+//         changeLanguageLocalStr("EN");
+//         changeLanguageClass("en");
+// }
+// }
+
+// function changeLanguageClass(language) {
+//     if (language === 'ua') {
+//         document.body.classList.remove("ua");
+//         document.body.classList.add("en");
+//     } else {
+//         document.body.classList.remove("en");
+//         document.body.classList.add("ua");
+//     }
+// }
+
+// function changeLanguageLocalStr(language) {
+//     if (language === 'UA') {  
+//         saveDataToLocalSt("language", "EN");
+//     } else {  
+//         saveDataToLocalSt("language", "UA");
+// }
+// }
+
+// function changeLanguageText(language) {
+//     if (language === 'UA') {
+//         dataLanguage.forEach((data) => {
+//             data.textContent = data.dataset.enlang;
+//             if (data.hasAttribute("placeholder")) {
+//                 data.placeholder = data.dataset.enlang;
+//             }
+//         }); 
+
+//     } else { 
+//         dataLanguage.forEach((data) => {
+//             data.textContent = data.dataset.ualang;
+//             if (data.hasAttribute("placeholder")) {
+//                 data.placeholder = data.dataset.ualang;
+//             }
+//         });
+// }
+// }
--- src/js/switcher_theme.js
+++ src/js/switcher_theme.js
@@ -1,11 +1,15 @@
 import { saveDataToLocalSt, loadDataFromLocalSt, removeDataFromLocalSt } from './tool-tip';
 
-const refs = {
+class ThemeSwitch {
+    constructor( theme ) {
+    this.refs = {
     toggleLang: document.querySelector('.theme-switch__toggle'),
-};
+    };
+}
 
-const storageTheme = loadDataFromLocalSt("theme");
-if (storageTheme !== undefined) {
+init() {
+    const storageTheme = loadDataFromLocalSt("theme");
+    if (storageTheme !== undefined) {
     if (storageTheme === "dark") {
         document.body.classList.remove("light-theme");
         document.body.classList.add("dark-theme");
@@ -13,23 +17,61 @@ if (storageTheme !== undefined) {
         document.body.classList.remove("dark-theme");
         document.body.classList.add("light-theme");
     }
-} else {
+    } else {
     document.body.classList.add("light-theme");
+    }
+    this.refs.toggleLang.addEventListener('change', this.changeTheme.bind(this));
 }
 
-refs.toggleLang.addEventListener('change', changeTheme);
-
-
-function changeTheme(evt) {
+changeTheme() {
     if (document.body.classList.contains("light-theme")) {
-        document.body.classList.remove("light-theme");
-        document.body.classList.add("dark-theme");
-        saveDataToLocalSt("theme", "dark");
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+    saveDataToLocalSt("dark");
     } else {
-        document.body.classList.remove("dark-theme");
-        document.body.classList.add("light-theme");
-        saveDataToLocalSt("theme", "light");
+    document.body.classList.remove("dark-theme");
+    document.body.classList.add("light-theme");
+    saveDataToLocalSt("light");
     }
 }
+saveDataToLocalSt(theme) {
+    saveDataToLocalSt("theme", theme);
+}
+}
+
+const themeSwitch = new ThemeSwitch();
+themeSwitch.init();
+
+// const refs = {
+//     toggleLang: document.querySelector('.theme-switch__toggle'),
+// };
+
+// const storageTheme = loadDataFromLocalSt("theme");
+// if (storageTheme !== undefined) {
+//     if (storageTheme === "dark") {
+//         document.body.classList.remove("light-theme");
+//         document.body.classList.add("dark-theme");
+//     } else {
+//         document.body.classList.remove("dark-theme");
+//         document.body.classList.add("light-theme");
+//     }
+// } else {
+//     document.body.classList.add("light-theme");
+// }
+
+// refs.toggleLang.addEventListener('change', changeTheme);
+
+
+// function changeTheme(evt) {
+//     if (document.body.classList.contains("light-theme")) {
+//         document.body.classList.remove("light-theme");
+//         document.body.classList.add("dark-theme");
+//         saveDataToLocalSt("theme", "dark");
+//     } else {
+//         document.body.classList.remove("dark-theme");
+//         document.body.classList.add("light-theme");
+//         saveDataToLocalSt("theme", "light");
+//     }
+// }
 
 export { refs, changeTheme };
