f485e3d2776a1c058584fcbf40ce604cc7a63f3e
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-12-23 23:40:39 +0200

    fixex get requests

--- src/js/manipulation-with-api/get-latest-movies.js
+++ src/js/manipulation-with-api/get-latest-movies.js
@@ -6,7 +6,6 @@ import { renderPagination } from '../utils/pagination';
 const api = new API();
 
 const moviesList = document.querySelector('.movies-grid__list');
-const pagination = document.querySelector('.pagination-list');
 
 moviesList.addEventListener('click', onMovieClick);
 
@@ -18,7 +17,7 @@ async function getLatestMovies() {
   const template = (await Promise.all(getPromise)).join('');
 
   moviesList.innerHTML = template;
-  renderPagination(movies.total_pages, pagination, getLatestMovies, api);
+  renderPagination(movies.total_pages, getLatestMovies, api);
 }
 
 async function createMovieData() {
