5b34f66b05e1166596fc45582c1a428ac52781d0
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-12-24 14:16:09 +0200

    add styles for movie modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,41 +1,66 @@
-import { Modal } from './js/class-modal';
+export const getModalMarkup = ({
+  overview,
+  vote_average,
+  popularity,
+  original_title,
+  genres,
+  poster_path,
+  vote_count,
+  title,
+  id,
+}) => {
+  const url = `https://image.tmdb.org/t/p/original${poster_path}`;
+  const genresNames = genres.map(genre => genre.name).join(', ');
 
-const itemCardEl = document.querySelector('.movie-card');
-            
-const modal = new Modal(option);
+  return `<div class="modal-movie" data-id='${id}'>
+  <img class="poster" width="375" src="${url}" alt="${title}" />
+  <div class="modal-movie__descr">   
+    
+    <div class="modal-movie__info-weapper">
+    <h2 class="modal-movie__title">${title}</h2>  
+    <ul class="modal-movie__list list">
+    <li class="modal-movie__item">
+    <p class="modal-movie__key">Vote / Votes</p>
+     <p class="modal-movie__value">
+       <span class="modal-movie__value--accent">${vote_average}</span> / ${vote_count}
+     </p>
+    </li>
+    <li class="modal-movie__item">
+     <p class="modal-movie__key">Popularity</p>
+     <p class="modal-movie__value">${popularity}</p>
+    </li>
+    <li class="modal-movie__item">
+    <p class="modal-movie__key">Original Title</p>
+     <p class="modal-movie__value modal-movie__value--uppercase">
+     ${original_title}</p>
+    </li>
+    <li class="modal-movie__item">
+    <p class="modal-movie__key">Genre</p>
+     <p class="modal-movie__value">${genresNames}</p>
+    </li>
+    
+    </ul>  
+      <div class="modal-movie__about-wrapper">
+      <p class="modal-movie__about">About</p>
+      <p class="modal-movie__overview">${overview}</p>
+      </div>
+      
+      <div class="trailer-wrapper"></div>
+       <ul class="buttons-list buttons-list__film-modal">
+     <li class="buttons-list__item">
+         <button class="button button--transparent" type="button" data-list="watched">
+             add to Watched
+         </button>
+     </li>
+     <li class="buttons-list__item">
+         <button class="button button--transparent" type="button" data-list="queue">
+             add to queue
+         </button>
+     </li>
 
-const url = `https://image.tmdb.org/t/p/original${poster_path}`;
-
-const getModalMarkup = ({ overview, vote_average, popularity, original_title, genres, poster_path, vote_count, title }) => {
-  const genresNames = genres.map(genre => genre.name).join(' ');
-            return `
-              <img src="${url}${poster_path}" alt="film photo" class="modal-card-film-img">
-              <div>
-              <h2 class="modal-card__title">${title}</h2>
-              <div class="modal-card-list-wrapper">
-                <ul class="modal-list-info">
-                  <li class="modal-card__item"><span class="modal-card__info">Vote / Votes</span>
-                    <div class="modal-card__value">
-                      <span class="modal-card__vote">${vote_average}</span>
-                      <span class="modal-card__slash">/</span>
-                      <span class="modal-card__value">${vote_count}</span>
-                    </div>
-                  </li>
-                  <li class="modal-card__item"><span class="modal-card__info">Popularity</span><span
-                      class="modal-card__value">${popularity}</span></li>
-                  <li class="modal-card__item"><span class="modal-card__info">Original Title</span
-                      class="modal-card__value">${original_title}<span></span></li>
-                  <li class="modal-card__item"><span class="modal-card__info">Genre</span><span
-                      class="modal-card__value">${genresNames.genre}</span></li>
-                </ul>
-              </div>
-              <h3 class="modal-card-title-about">About</h3>
-              <p class="modal-card-about-text">${overview}</p>
-              <button class="modal-card-watched-btn" type="button">add to watched</button>
-              <button type="button" class="modal-card-queue-btn">add to queue</button>
-              </div>`;
-             
-}
-
-
-modal.open(getModalMarkup());
+     </ul>
+     <p class='modal-movie__notify form__error-notification--for-header'></p>
+    </div>
+  </div>
+</div>`;
+};
