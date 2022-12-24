b6e287835c188ecec71fe464004f168063d1633f
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-24 10:40:29 +0200

    Fix markup

--- src/js/create-movie-card.js
+++ src/js/create-movie-card.js
@@ -20,8 +20,8 @@ export async function createMovieCardMarkup({
   release_date,
 }) {
   const genre = await searchGenres(genre_ids);
-
-  if (genre_ids === 0) {
+  debugger;
+  if (!genre_ids) {
     return await `<li class="movie-card" id="${id}">
         <img 
         src="https://image.tmdb.org/t/p/original${poster_path}" 
@@ -39,7 +39,7 @@ export async function createMovieCardMarkup({
               </div>
           </div>
       </li>`;
-  } else {
+  } else if (!release_date) {
     return await `<li class="movie-card" id="${id}">
         <img 
         src="https://image.tmdb.org/t/p/original${poster_path}" 
@@ -49,11 +49,39 @@ export async function createMovieCardMarkup({
             <p class="movie-card__name">${title}</p>
             <div class="movie-card__wrap">
             <p class="movie-card__info-wrap">
+              <span class="movie-card__genre">${genre}</span> |
+              </p>
+              <span class="movie-card__rating">${vote_average.toFixed(1)}</span>
+              </div>
+          </div>
+      </li>`;
+  } else if (!genre_ids && !release_date) {
+    return await `<li class="movie-card" id="${id}">
+        <img 
+        src="https://image.tmdb.org/t/p/original${poster_path}" 
+        alt="Poster of ${title}" class="movie-card__img" />
+        
+          <div class="movie-card__info">
+            <p class="movie-card__name">${title}</p>
+            <div class="movie-card__wrap">
+              <span class="movie-card__rating">${vote_average.toFixed(1)}</span>
+              </div>
+          </div>
+      </li>`;
+  } else {
+    return await `<li class="movie-card" id="${id}">
+    <img src="https://image.tmdb.org/t/p/original${poster_path}" 
+        alt="Poster of ${title}" class="movie-card__img" />
+          <div class="movie-card__info">
+            <p class="movie-card__name">${title}</p>
+            <div class="movie-card__wrap">
+            <p class="movie-card__info-wrap">
               <span class="movie-card__genre">${genre}</span> | <span class="movie-card__release">${
       release_date.split('-')[0]
     }</span>
-              </p>
-              <span class="movie-card__rating">${vote_average.toFixed(1)}</span>
+              <span class="movie-card__rating">${vote_average.toFixed(
+                1
+              )}</span></p>
               </div>
           </div>
       </li>`;
